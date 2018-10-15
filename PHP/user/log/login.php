ebf26d0acdc8ec37acd09fafee2b4b35e25b159c
Adrian Reyes <adriandmc05@gmail.com>
2018-10-15 15:08:16 -0400

    Adrian lo mama

--- PHP/user/log/login.php
+++ PHP/user/log/login.php
@@ -2,21 +2,40 @@
 <html>
 <head>
 	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="../../../css/login.css">
+	<!-- <link rel="stylesheet" type="text/css" href="../../../css/login.css"> -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 </head>
 <body>
 	
-	<h1 class="titulo">Cable Unet</h1>
-
 	<div class="container">
-		<form id="loginform" method="post" action="../config_user/login_db.php">
-			<input type="text" name="user" class="input" placeholder="Usuario"  required="" /> 
-			<input type="password" name="clave" class="input" placeholder="Contraseña" required="" />
-			<input type="submit" name="enviar" class="loginbutton" value="Enviar" />
-		</form>
+		<div class="row">
+			<div class="col">			
+				<h1 class="text-center">Cable Unet</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-6 offset-0 offset-md-3 border border-dark p-4">				
+				<form method="post" action="../config_user/login_db.php">
+					<div class="form-group">
+						<label for="user">Usuario</label>
+						<input type="text" class="form-control" name="user" id="user" placeholder="Usuario">
+						<small class="form-text text-muted">Imgresa el usuario con el que deseas registrarte</small>
+					</div>
+					<div class="form-group">
+						<label for="password">Contraseña</label>
+						<input type="password" class="form-control" id="password" placeholder="Password" name="clave">
+					</div>
+					
+					<button type="submit" class="btn btn-primary">Ingresar</button>
+					<a class="btn btn-secondary" href="registro.php">Registrate</a>
+				</form>
+			</div>
+		</div>
 	</div>
+
+	
 		
-	<a href="registro.php">Registrate</a>
 
 </body>
 </html>
